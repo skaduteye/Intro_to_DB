d888815dbe3f129d64950bc07fb81f55bff267df
skaduteye <skaduteye@gmail.com>
2025-10-19 22:27:50 +0000

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,5 @@
--- Display the full description of the table books
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- Print the full description of the table books
 SHOW CREATE TABLE Books;
